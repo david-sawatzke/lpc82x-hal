76cb35f1782b98cc6b50a23212ace738a8c7e938
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-05-07 19:21:10 +0200

    Add FRO

--- lpc845-hal/src/syscon.rs
+++ lpc845-hal/src/syscon.rs
@@ -9,12 +9,16 @@
 //!
 //! The SYSCON peripheral is described in the user manual, chapter 5.
 
-use crate::common::syscon::CommonParts;
+use core::marker::PhantomData;
+
 pub use crate::common::syscon::{
-    AnalogBlock, BodWakeup, ClockControl, Handle, I2c0Wakeup, I2c1Wakeup, I2c2Wakeup, I2c3Wakeup,
-    ResetControl, Spi0Wakeup, Spi1Wakeup, Usart0Wakeup, Usart1Wakeup, Usart2Wakeup, WktWakeup,
-    WwdtWakeup, BOD, FLASH, MTB, RAM0_1, ROM, SYSOSC, SYSPLL,
+    impl_analog_block, AnalogBlock, BodWakeup, ClockControl, Handle, I2c0Wakeup, I2c1Wakeup,
+    I2c2Wakeup, I2c3Wakeup, ResetControl, Spi0Wakeup, Spi1Wakeup, Usart0Wakeup, Usart1Wakeup,
+    Usart2Wakeup, WktWakeup, WwdtWakeup, BOD, FLASH, MTB, RAM0_1, ROM, SYSOSC, SYSPLL,
 };
+
+use common::{clock, syscon::CommonParts};
+use reg_proxy::RegProxy;
 
 /// Entry point to the SYSCON API
 ///
@@ -55,11 +59,15 @@ impl SYSCON {
             handle: parts.handle,
             bod: parts.bod,
             flash: parts.flash,
+            fro: FRO(PhantomData),
+            froout: FROOUT(PhantomData),
             mtb: parts.mtb,
             ram0_1: parts.ram0_1,
             rom: parts.rom,
             sysosc: parts.sysosc,
             syspll: parts.syspll,
+
+            fro_derived_clock: FroDerivedClock::new(),
         }
     }
 
@@ -96,6 +104,12 @@ pub struct Parts {
     /// Flash memory
     pub flash: FLASH,
 
+    /// FRO
+    pub fro: FRO,
+
+    /// FRO
+    pub froout: FROOUT,
+
     /// Micro Trace Buffer
     pub mtb: MTB,
 
@@ -110,4 +124,83 @@ pub struct Parts {
 
     /// PLL
     pub syspll: SYSPLL,
+
+    /// The 750 kHz FRO-derived clock
+    pub fro_derived_clock: FroDerivedClock<init_state::Enabled>,
 }
+
+/// FRO
+///
+/// Can be used to control FRO output using various methods on
+/// [`syscon::Handle`].
+///
+/// [`syscon::Handle`]: struct.Handle.html
+pub struct FRO(PhantomData<*const ()>);
+
+/// FRO  output
+///
+/// Can be used to control FRO output using various methods on
+/// [`syscon::Handle`].
+///
+/// [`syscon::Handle`]: struct.Handle.html
+pub struct FROOUT(PhantomData<*const ()>);
+
+/// The 750 kHz FRO-derived clock
+///
+/// This is one of the clocks that can be used to run the self-wake-up timer
+/// (WKT). See user manual, section 23.5.1.
+pub struct FroDerivedClock<State = init_state::Enabled> {
+    _state: State,
+}
+
+impl FroDerivedClock<init_state::Enabled> {
+    pub(crate) fn new() -> Self {
+        FroDerivedClock {
+            _state: init_state::Enabled(()),
+        }
+    }
+}
+
+impl FroDerivedClock<init_state::Disabled> {
+    /// Enable the FRO-derived clock
+    ///
+    /// This method is only available, if `FroDerivedClock` is in the
+    /// [`Disabled`] state. Code that attempts to call this method when the
+    /// clock is already enabled will not compile.
+    ///
+    /// Consumes this instance of `FroDerivedClock` and returns another instance
+    /// that has its `State` type parameter set to [`Enabled`]. That new
+    /// instance implements [`clock::Enabled`], which might be required by APIs
+    /// that need an enabled clock.
+    ///
+    /// Also consumes the handles to [`FRO`] and [`FROOUT`], to make it
+    /// impossible (outside of unsafe code) to break API guarantees.
+    ///
+    /// [`Disabled`]: ../init_state/struct.Disabled.html
+    /// [`Enabled`]: ../init_state/struct.Enabled.html
+    /// [`clock::Enabled`]: ../clock/trait.Enabled.html
+    pub fn enable(
+        self,
+        syscon: &mut Handle,
+        mut fro: FRO,
+        mut froout: FROOUT,
+    ) -> FroDerivedClock<init_state::Enabled> {
+        syscon.power_up(&mut fro);
+        syscon.power_up(&mut froout);
+
+        FroDerivedClock {
+            _state: init_state::Enabled(()),
+        }
+    }
+}
+
+impl<State> clock::Frequency for FroDerivedClock<State> {
+    fn hz(&self) -> u32 {
+        750_000
+    }
+}
+
+impl clock::Enabled for FroDerivedClock<init_state::Enabled> {}
+
+impl_analog_block!(FRO, fro_pd);
+impl_analog_block!(FROOUT, froout_pd);
